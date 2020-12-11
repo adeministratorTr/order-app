07bbb0990af13f85a68ed23857cfafbc594543be
adeministratorTr <adembavas@gmail.com>
2020-12-11 19:35:47 +0300

    FE - feat: Add main page

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -1,9 +1,49 @@
+import Link from 'next/link'
+import { Row, Col, Button, InputGroup, FormControl } from 'react-bootstrap';
+
+import Search from '../assets/icons/search'
+import URL_LIST from '../pages/constants/url-list'
 import Layout from '../components/layout';
 
 export default function App() {
   return (
     <Layout>
-      <h1>The challenge content goes here.</h1>
+      <Row className="justify-content-md-center" style={{ marginBottom: '32px' }}>
+        <h1>Help Center</h1>
+      </Row>
+      <Row>
+        <Col className="justify-content-sm-center" xs={12} md={12}>
+          <InputGroup className="mb-3">
+            <InputGroup.Prepend>
+              <InputGroup.Text>
+                <Search />
+              </InputGroup.Text>
+            </InputGroup.Prepend>
+            <FormControl
+              placeholder="Search"
+              aria-label="Search"
+              aria-describedby="basic-addon1"
+            />
+          </InputGroup>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12} md={5} style={{ marginBottom: '32px' }}>
+          <Link href={URL_LIST.CONTACT_FORM}>
+            <Button variant="primary" size="lg" block variant="outline-primary">
+              Contact Form
+            </Button>
+          </Link>
+        </Col>
+        <Col md={2} />
+        <Col xs={12} md={5} style={{ marginBottom: '32px' }}>
+          <Link href={URL_LIST.ORDER_DETAIL}>
+            <Button variant="primary" size="lg" block variant="outline-primary">
+              Where is my order?
+            </Button>
+          </Link>
+        </Col>
+      </Row>
     </Layout>
   );
 };
