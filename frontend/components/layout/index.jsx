38f320ec5c2af36affcadc7d9393a098fc69607d
adeministratorTr <adembavas@gmail.com>
2020-12-11 18:21:01 +0300

    FE - feat: Add page links to logo and contact us on Layout

--- frontend/components/layout/index.jsx
+++ frontend/components/layout/index.jsx
@@ -1,44 +1,49 @@
+import Link from 'next/link'
+
 import {
-        mainHeader,
-        mainFooter,
-        mainContent,
-        headerContent,
-        layoutBody,
-        headerSvg,
-        footerContactUs,
-        footerSvg
+  mainHeader,
+  mainFooter,
+  mainContent,
+  headerContent,
+  layoutBody,
+  headerSvg,
+  footerContactUs,
+  footerSvg
 } from './layout.module.css';
 
 import TestLogo from './TestLogo';
+import URL_LIST from '../../pages/constants/url-list'
 
-export default function Layout (props) {
-    return (
+export default function Layout(props) {
+  return (
     <div className={layoutBody}>
-        <header className={mainHeader}>
+      <header className={mainHeader}>
         <svg className={headerSvg} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none">
-            <polygon fill="white" points="0,100 100,0 100,100"/>
+          <polygon fill="white" points="0,100 100,0 100,100" />
         </svg>
-        <div className={headerContent}>
+        <Link href={URL_LIST.INDEX}>
+          <div className={headerContent}>
             <TestLogo />
-        </div>
+          </div>
+        </Link>
+      </header>
 
-        </header>
-
-        <div className={mainContent}>
+      <div className={mainContent}>
         {props.children}
-        </div>
-        <div className={footerContactUs}>
+      </div>
+
+      <div className={footerContactUs}>
         <svg className={footerSvg} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none">
-            <polygon fill="white" points="100,0 0,0 0,100"/>
+          <polygon fill="white" points="100,0 0,0 0,100" />
         </svg>
         <div>
-        <h3>Couldn't find what you are looking for?</h3>
-        <a href="">Contact us directly</a>
+          <h3>Couldn't find what you are looking for?</h3>
+          <a href={URL_LIST.CONTACT_FORM}>Contact us directly</a>
         </div>
-        </div>
-        <footer className={mainFooter}>
-            test.com
-        </footer>
+      </div>
+      <footer className={mainFooter}>
+        test.com
+      </footer>
     </div>
-    );
+  );
 }
---
+++ frontend/pages/constants/url-list.js
@@ -0,0 +1,8 @@
+const URL_LIST = {
+  INDEX: '/',
+  CONTACT_FORM: '/contact-form',
+  ARTICLES: '/articles',
+  ORDER_DETAIL: '/order-detail'
+}
+
+export default URL_LIST
