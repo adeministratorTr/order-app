f602aae03dbb94841a14012d6ab9e1d440530fc4
adeministratorTr <adembavas@gmail.com>
2020-12-11 18:35:43 +0300

    FE - feat: Add contact form & order details pages initial version

---
+++ frontend/pages/contact-form/index.jsx
@@ -0,0 +1,9 @@
+import Layout from '../../components/layout'
+
+export default function ContactForm() {
+  return (
+    <Layout>
+      <p>Contact Form page</p>
+    </Layout>
+  )
+}
---
+++ frontend/pages/order-detail/index.jsx
@@ -0,0 +1,9 @@
+import Layout from '../../components/layout'
+
+export default function OrderDetail() {
+  return (
+    <Layout>
+      <p>Order Detail page</p>
+    </Layout>
+  )
+}
