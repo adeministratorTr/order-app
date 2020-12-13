e09d75d3e7e3f6c3a20a38adcdabf95b6e091ff6
adeministratorTr <adembavas@gmail.com>
2020-12-13 15:50:57 +0300

    BE - fix: Fix cors errors

--- backend/src/app.mjs
+++ backend/src/app.mjs
@@ -1,7 +1,12 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
 import * as config from './config';
 
 const app = express();
+app.use(bodyParser.json())
+app.use(cors())
 
 app.listen(config.PORT);
 
