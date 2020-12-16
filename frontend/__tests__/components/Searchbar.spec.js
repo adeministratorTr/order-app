44dc3dc797042b30e61e50c17b034577f6956880
adeministratorTr <adembavas@gmail.com>
2020-12-16 20:09:48 +0300

    FE - test: Add searchBar component test

---
+++ frontend/__tests__/components/Searchbar.spec.js
@@ -0,0 +1,23 @@
+import { render, fireEvent } from '@testing-library/react'
+
+import SearchBar from '../../components/searchBar'
+
+describe('components/SearchBar tests', () => {
+  it('should render default', () => {
+    const { getByLabelText } = render(<SearchBar />)
+    const searchBar = getByLabelText('search-bar-input')
+    expect(searchBar).toBeDefined()
+  })
+
+  it('should render new value', () => {
+    const onChangeMock = jest.fn()
+    const { getByLabelText } = render(<SearchBar onChange={onChangeMock} />)
+    const searchBar = getByLabelText('search-bar-input')
+    const searchBarText = 'my orders'
+
+    expect(searchBar.value).toBe('')
+    fireEvent.change(searchBar, { target: { value: searchBarText } })
+    expect(onChangeMock).toHaveBeenCalledTimes(1)
+    expect(searchBar.value).toBe(searchBarText)
+  })
+})
